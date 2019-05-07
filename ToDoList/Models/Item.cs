da3d3b8b166535f34d34a69e7ddaa3dae9d9ff8c
John <john@epicodus.com>
2019-05-07 14:11:30 -0700

    set up project for export

---
+++ ToDoList/Models/Item.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
+namespace ToDoList.Models
+{
+    public class Item
+    {
+        public string Description { get; set; }
+        public int Id { get; }
+
+        public Item(string description)
+        {
+            Description = description;
+        }
+        public Item(string description, int id)
+        {
+            Description = description;
+            Id = id;
+        }
+
+        public static List<Item> GetAll()
+        {
+            List<Item> allItems = new List<Item> { };
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"SELECT * FROM items;";
+            MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+            while (rdr.Read())
+            {
+                int itemId = rdr.GetInt32(0);
+                string itemDescription = rdr.GetString(1);
+                Item newItem = new Item(itemDescription, itemId);
+                allItems.Add(newItem);
+            }
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+            return allItems;
+        }
+
+        public static void ClearAll()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"DELETE FROM items;";
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        } 
+
+        // public static Item Find(int searchId)
+        // {
+        //     return _instances[searchId - 1];
+        // }
+    }
+}
