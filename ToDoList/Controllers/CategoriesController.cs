862542da99e60f0bad3be08037b9cb7b561f0103
John Franti <jfranti@gmail.com>
2019-05-09 16:29:29 -0700

    update 1-to-many

---
+++ ToDoList/Controllers/CategoriesController.cs
@@ -0,0 +1,65 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using ToDoList.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDoList.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private ToDoListContext db = new ToDoListContext();
+        public IActionResult Index()
+        {
+            List<Category> model = db.Categories.ToList();
+            return View(model);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Category thisCategory = db.Categories.Include(x => x.Items).FirstOrDefault(Categories => Categories.CategoryId == id);
+            return View(thisCategory);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            db.Categories.Add(category);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            var thisItem = db.Categories.FirstOrDefault(categories => categories.CategoryId == id);
+            return View(thisItem);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            db.Entry(category).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var thisCategory = db.Categories.FirstOrDefault(categories => categories.CategoryId == id);
+            return View(thisCategory);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisCategory = db.Categories.FirstOrDefault(categories => categories.CategoryId == id);
+            db.Categories.Remove(thisCategory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -6,8 +6,11 @@ namespace ToDoList.Models
     [Table("items")]
     public class Item
     {
-        public string Description { get; set; }
+        [Key]
         public int ItemId { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
 
     }
 }
